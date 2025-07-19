826767e7e79987fa09dbb5604f4d85c55be7030b
unknown <nzshivap@gmail.com>
2025-07-19 23:24:36 +1200

    update

--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,11 @@ output "rds_endpoint" {
 }
 
 output "ec2_security_group_id" {
-  value = aws_security_group.ec2_sg.id
+  value = aws_security_group.ec21_sg.id
   description = "Security group ID for EC2."
 }
 
 output "rds_security_group_id" {
-  value = aws_security_group.rds_sg.id
+  value = aws_security_group.rds1_sg.id
   description = "Security group ID for RDS."
 }
